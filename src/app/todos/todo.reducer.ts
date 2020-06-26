5eeac57f5eb85450f5a6f8741dbc2568f31519c2
Luis Alberto López Lombana <llombana@stratio.com>
2020-06-26 16:42:38 -0500

    Creación de componente TODOs

---
+++ src/app/todos/todo.reducer.ts
@@ -0,0 +1,53 @@
+import { createReducer, on } from '@ngrx/store';
+import { crear, toggle, editar, borrar, toogleAll, limpiarTodos } from './todo.actions';
+import { Todo } from './models/todo.model';
+
+
+export const estadoInicial: Todo[] = [
+    new Todo('Salvar al mundo'),
+    new Todo('Vencer a Thanos'),
+    new Todo('Comprar traje de Ironman'),
+    new Todo('Robar escudo del Capitán América'),
+];
+
+const _todoReducer = createReducer(estadoInicial,
+  on(crear, (state, { texto }) => [...state, new Todo( texto )] ),
+  on(borrar, (state, {id}) => state.filter(todo => todo.id !== id)),
+  on(limpiarTodos, (state) => state.filter(todo => !todo.completado)),
+  on(toggle, (state, { id }) => {
+    return state.map(todo => {
+      if ( todo.id === id ){
+        return {
+          ...todo,
+          completado: !todo.completado
+        }
+      }else{
+        return todo;
+      }
+    });
+  }),
+  on(editar, (state, { id, texto }) => {
+    return state.map(todo => {
+      if ( todo.id === id ){
+        return {
+          ...todo,
+          texto: texto
+        }
+      }else{
+        return todo;
+      }
+    });
+  }),
+  on(toogleAll, (state, { completado }) => {
+    return state.map(todo => {
+       return {
+          ...todo,
+          completado: completado
+        }
+    });
+  }),
+);
+
+export function todoReducer(state, action) {
+  return _todoReducer(state, action);
+}
